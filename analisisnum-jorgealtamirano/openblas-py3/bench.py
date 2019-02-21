8ce581580cb0b7da87d9ebab7c7d8f8314925d17
philwebsurfer <phil_websurfer@yahoo.com>
2019-02-21 14:56:28 -0600

    Minor fixes to python benchmarking scripts

--- analisisnum-jorgealtamirano/openblas-py3/bench.py
+++ analisisnum-jorgealtamirano/openblas-py3/bench.py
@@ -1,72 +1,100 @@
 #!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
 # @author: Jorge Altamirano, Ariel Vallarino, Eduardo Martínez 2019
 # @description: Ejecuta pruebas de benchmarking en punto flotante, basado en numpy con fines comparativos.
 
-# Basado en http://stackoverflow.com/questions/11443302/compiling-numpy-with-openblas-integration
-
+import pandas as pd
 import numpy as np
 from time import time
 
-print("==========================================================================")
-print("Valores de la configuracion OpenBLAS")
-np.__config__.show()
-print("========================================================================\n")
+df = pd.DataFrame(columns=['BLAS', 'Tamaño', 'Operación', 'Iteraciones', 'Tiempo'])
 
-# Establecer la semilla con fines de reproducibilidad
 np.random.seed(0)
 
-# Tamaño d ela matriz
-size = 4096
-# Declaracion de las matrices
-A, B = np.random.random((size, size)), np.random.random((size, size))
-C, D = np.random.random((size * 128,)), np.random.random((size * 128,))
-E = np.random.random((int(size / 2), int(size / 4)))
-F = np.random.random((int(size / 2), int(size / 2)))
-#Producto punto
-F = np.dot(F, F.T)
-G = np.random.random((int(size / 2), int(size / 2)))
+for i in range(6, 12):
+    # Multiplicacion de matriz 20 veces (N) para sacar un promedio.
+    size = 2**i
+    A, B = np.random.random((size, size)), np.random.random((size, size))
+    N = 5
+    t = time()
+    for i in range(N):
+        np.dot(A, B)
+    delta = time() - t
+    del A, B
+    df = df.append({'BLAS': np.__config__.blas_opt_info['libraries'][0],
+               'Tamaño': size, 
+               'Operación': "Producto Punto de Matrices", 
+               'Iteraciones': N,
+               'Tiempo': delta/N}, 
+              ignore_index=True)
+    
+for i in range(8, 14):
+    # Multiplicacion de vectores 5000 Veces para sacar un promedio.
+    size = 2**i
+    N = 2500
+    C, D = np.random.random((size * 128,)), np.random.random((size * 128,))
+    t = time()
+    for i in range(N):
+        np.dot(C, D)
+    delta = time() - t
+    df = df.append({'BLAS': np.__config__.blas_opt_info['libraries'][0],
+               'Tamaño': (size*128), 
+               'Operación': "Producto Punto de 2 Vectores", 
+               'Iteraciones': N,
+               'Tiempo': delta/N}, 
+              ignore_index=True)
+    del C, D
+    
+for i in range(6, 12):
+    # SVD: promedio de 3.
+    size = 2**i
+    E = np.random.random((int(size), int(size/2)))
+    N = 5
+    t = time()
+    for i in range(N):
+        np.linalg.svd(E, full_matrices = False)
+    delta = time() - t
+    df = df.append({'BLAS': np.__config__.blas_opt_info['libraries'][0],
+           'Tamaño': (size),
+           'Operación': "SVD", 
+           'Iteraciones': N,
+           'Tiempo': delta/N}, 
+          ignore_index=True)
+    del E
+    
+for i in range(6, 12):
+    # Cholesky: promedio de 3.
+    size = 2**i
+    F = np.random.random((int(size / 2), int(size / 2)))
+    F = np.dot(F, F.T)
+    N = 5
+    t = time()
+    for i in range(N):
+        np.linalg.cholesky(F)
+    delta = time() - t
+    df = df.append({'BLAS': np.__config__.blas_opt_info['libraries'][0],
+       'Tamaño': size/2,
+       'Operación': "Cholesky", 
+       'Iteraciones': N,
+       'Tiempo': delta/N}, 
+      ignore_index=True)
+    del F
 
-# Multiplicacion de matriz 20 veces (N) para sacar un promedio.
-N = 20
-t = time()
-for i in range(N):
-    np.dot(A, B)
-delta = time() - t
-print('Producto punto de 2 matrices %dx%d tomo %0.2f segundos en promedio. ' % (size, size, delta / N))
-del A, B
-
-# Multiplicacion de vectores 5000 Veces para sacar un promedio.
-N = 5000
-t = time()
-for i in range(N):
-    np.dot(C, D)
-delta = time() - t
-print('Producto punto de 2 vectores de longitud %d tomo %0.2f ms en promedio.' % (size * 128, 1e3 * delta / N))
-del C, D
-
-# SVD: promedio de 3.
-N = 3
-t = time()
-for i in range(N):
-    np.linalg.svd(E, full_matrices = False)
-delta = time() - t
-print("SVD de una matriz %dx%d tomo %0.2f segundos." % (size / 2, size / 4, delta / N))
-del E
-
-# Cholesky: promedio de 3.
-N = 3
-t = time()
-for i in range(N):
-    np.linalg.cholesky(F)
-delta = time() - t
-print("Descomposicion de una Cholesky de una matriz %dx%d tomo  %0.2f segundos." % (size / 2, size / 2, delta / N))
-
-# Valores y vectores propios: promedio de 3.
-t = time()
-for i in range(N):
-    np.linalg.eig(G)
-delta = time() - t
-print("Descompisicion de valores y vectores propios de una matriz %dx%d tomo %0.2f segundos." % (size / 2, size / 2, delta / N))
-
-
+for i in range(6, 12):
+    # Valores y vectores propios: promedio de 3.
+    size = 2**i
+    t = time()
+    G = np.random.random((int(size / 2), int(size / 2)))
+    N = 5
+    for i in range(N):
+        np.linalg.eig(G)
+    delta = time() - t
+    df = df.append({'BLAS': np.__config__.blas_opt_info['libraries'][0],
+       'Tamaño': size/2,
+       'Operación': "Eigen", 
+       'Iteraciones': N,
+       'Tiempo': delta/N}, 
+      ignore_index=True)
+    del G
+filename = "compare.%s.csv"%(np.__config__.blas_opt_info['libraries'][0])
+print("Guardando archivo en " + filename)
+df.to_csv(filename, encoding="utf-8", index=False)
